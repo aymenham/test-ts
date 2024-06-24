3888840e43f29fcc39a8460ec0ba98b81db856e8
Aimene Hamidechi <a.hamidechi@clicar.fr>
2024-06-24 16:59:20 +0200

    commit

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,14 +1,13 @@
 import React from "react";
 import "./App.css";
-import Test1 from "./Test1/Test2";
 import Test3 from "./Test3/Test3";
+import Test4 from "./Test4/Test4";
 
 function App() {
   return (
     <div className="App">
-      {/* <Test1 /> */}
-
-      {/* <Test3 /> */}
+      {/*   <Test3 /> */}
+      {/*   <Test4 /> */}
     </div>
   );
 }
